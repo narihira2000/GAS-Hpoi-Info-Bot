332fce0982143483015dd44c50a8b50ddecc3666
narihira2000 <narihira2000@gmail.com>
2025-02-05 20:10:15 +0800

    bump flask version to 3.x

--- line_notify/app.py
+++ line_notify/app.py
@@ -1,29 +1,35 @@
-from flask import *
+from flask import Flask, request
 import os
-import requests
+import aiohttp
 import json
 import asyncio
 
-line_data_sheet = ""
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST', 'GET'])
-def index():
+async def index():
     if request.method == 'POST':
-        loop = asyncio.get_event_loop()
         output_data = request.get_json()
         data = output_data['output_data']
-        
-        r = requests.get(line_data_sheet + "?action=getTokensAndKeys")
-        userDatas = json.loads(r.text)
+
+        async with aiohttp.ClientSession() as session:
+            line_data_sheet = os.getenv('LINE_DATA_SHEET')
+            async with session.get(f"{line_data_sheet}?action=getTokensAndKeys") as response:
+                res_text = await response.text()
+                userDatas = json.loads(res_text)
 
         async def send_line_notify(data, userData):
-            url = "https://notify-api.line.me/api/notify"
-            caption = "\n【" + data["info_type"] + "】\n" + data["info_title"] + "\nhttps://www.hpoi.net/" + data["link_path"] + "?openExternalBrowser=1\n\nTags:" + data["tag"]
-            header = {'Authorization': 'Bearer ' + userData['token']}
-            outdata = {'message': caption, 'imageThumbnail': data["img_path"], 'imageFullsize': data["img_path"]}
-            res = await loop.run_in_executor(None, lambda: requests.post(url, headers=header, data=outdata))
+            async with aiohttp.ClientSession() as session:
+                url = "https://notify-api.line.me/api/notify"
+                caption = "\n【" + data["info_type"] + "】\n" + data["info_title"] + "\nhttps://www.hpoi.net/" + data["link_path"] + "?openExternalBrowser=1\n\nTags:" + data["tag"]
+                header = {'Authorization': 'Bearer ' + userData['token']}
+                outdata = {'message': caption, 'imageThumbnail': data["img_path"], 'imageFullsize': data["img_path"]}
+                try:
+                    async with session.post(url, headers=header, data=outdata) as resp:
+                        return resp.status
+                except aiohttp.ClientError:
+                    return 500
 
         tasks = []
         for userData in userDatas:
@@ -48,15 +54,15 @@ def index():
                 # 關鍵字清單無符合，黑名單有符合 => 不傳送通知
                 if (isFound == 0 and isBlack == 0 and ct < len(arr)) or (isFound == 0 and isBlack == 1):
                     continue
-            task = loop.create_task(send_line_notify(data, userData))
-            tasks.append(task)
+            tasks.append(send_line_notify(data, userData))
             
-        loop.run_until_complete(asyncio.wait(tasks))
-        return f'send line success'
+        await asyncio.gather(*tasks)
+        print("send line success")
+        return 'send line success'
 
     if request.method == 'GET':
         print("gettt")
-        return f'hello'
+        return 'hello'
 
-if __name__ == 'main':
+if __name__ == '__main__':
     app.run()
